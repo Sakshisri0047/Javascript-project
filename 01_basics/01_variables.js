66e648fe886a8190b089f5f7203873373b948857
Sakshisri0047 <116529467+Sakshisri0047@users.noreply.github.com>
2023-11-23 05:55:22 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=14435
+let accountEmail="sakshisri"
+var accoountPassword="123456"
+accountCity="Jaipur"
+accountEmail="shdh"
+accoountPassword="23455"
+accountCity="banglore"
+console.table([acoountId,accountEmail,accoountPassword,accountCity])
+
